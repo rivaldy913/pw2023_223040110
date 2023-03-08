6473ac9277b24f3fb094c87f4a12dda11e8dcc3a
rivaldy913 <rivaldynovyandp@gmail.com>
2023-03-08 08:57:16 +0700

    mengerjakan tugas 3

---
+++ tugas/tugas_3/3a.php
@@ -0,0 +1,28 @@
+<?php 
+    echo"<h4>Menghitung Luas Lingkaran</h4>";
+    function hitungLuasLingkaran($r)
+
+    {
+
+        $luas = 3.14 * $r * $r;
+        return $luas . " cm <sup>2</sup>";
+
+    }
+
+    echo "Jari-jari = 10 cm. <br>";
+    echo "Luas lingkaran = " . hitungLuasLingkaran(10) . "<br>";
+    echo "<hr>";
+
+    echo "<h4>Menghitung Keliling Lingkaran</h4>";
+    function hitungKelilingLingkaran($r)
+
+    {
+        $keliling = 2 * 3.14 * $r;
+        return $keliling . " cm ";
+    }
+
+    echo "Jari-jari = 20 cm. <br>";
+    echo "Luas lingkaran = " . hitungKelilingLingkaran(20) . "<br>";
+    echo "<hr>";
+
+?>
---
+++ tugas/tugas_3/3b.php
@@ -0,0 +1,17 @@
+<?php 
+
+function urutanAngka($angka)
+
+{
+    $awal = 1;
+    for ($i = 1; $i <= $angka; $i++) {
+        for ($j = 1; $j <= $i; $j++) {
+            echo $awal . " ";
+            $awal++;
+        }
+        echo "<br>";
+    }
+}
+urutanAngka(5);
+
+?>
